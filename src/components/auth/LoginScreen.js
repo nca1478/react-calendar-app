9bc7ecdb580ffe9b8381351f31f60b4498397fc6
Nelson Cadenas <nca1478@yahoo.com>
2022-03-03 18:15:56 -0400

    added: dispatch action startLogin

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,25 +1,55 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { startLogin } from '../../actions/auth'
+import useForm from '../../hooks/useForm'
 import './LoginScreen.css'
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch()
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    lEmail: '',
+    lPassword: '',
+  })
+  const { lEmail, lPassword } = formLoginValues
+
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    rName: '',
+    rEmail: '',
+    rPassword1: '',
+    rPassword2: '',
+  })
+  const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    dispatch(startLogin(lEmail, lPassword))
+  }
+
   return (
     <div className="container">
       <div className="row m-0 vh-100 justify-content-center align-items-center">
         <div className="col-md-6 login-form-1">
-          <h3>Ingreso</h3>
-          <form>
+          <h3>Login</h3>
+          <form onSubmit={handleLogin}>
             <div className="mb-3">
               <input
                 type="text"
                 className="form-control"
-                placeholder="Correo"
+                placeholder="Email"
+                id="lEmail"
+                name="lEmail"
+                value={lEmail}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="mb-3">
               <input
                 type="password"
                 className="form-control"
-                placeholder="Contraseña"
+                placeholder="Password"
+                name="lPassword"
+                value={lPassword}
+                onChange={handleLoginInputChange}
               />
             </div>
             <div className="mb-3">
@@ -29,27 +59,38 @@ export const LoginScreen = () => {
         </div>
 
         <div className="col-md-6 login-form-2">
-          <h3>Registro</h3>
+          <h3>Register</h3>
           <form>
             <div className="mb-3">
               <input
                 type="text"
                 className="form-control"
-                placeholder="Nombre"
+                placeholder="Name"
+                name="rName"
+                value={rName}
+                onChange={handleRegisterInputChange}
+                autoComplete="off"
               />
             </div>
             <div className="mb-3">
               <input
                 type="email"
                 className="form-control"
-                placeholder="Correo"
+                placeholder="Email"
+                name="rEmail"
+                value={rEmail}
+                onChange={handleRegisterInputChange}
+                autoComplete="off"
               />
             </div>
             <div className="mb-3">
               <input
                 type="password"
                 className="form-control"
-                placeholder="Contraseña"
+                placeholder="Password"
+                name="rPassword1"
+                value={rPassword1}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
@@ -57,12 +98,15 @@ export const LoginScreen = () => {
               <input
                 type="password"
                 className="form-control"
-                placeholder="Repita la contraseña"
+                placeholder="Confirm Password"
+                name="rPassword2"
+                value={rPassword2}
+                onChange={handleRegisterInputChange}
               />
             </div>
 
             <div className="mb-3">
-              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+              <input type="submit" className="btnSubmit" value="Register" />
             </div>
           </form>
         </div>
