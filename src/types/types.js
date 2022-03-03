432c15cad00180b9b4e71f3e11ea6210e067435c
Nelson Cadenas <nca1478@yahoo.com>
2022-03-03 17:50:40 -0400

    added: authReducer

--- src/types/types.js
+++ src/types/types.js
@@ -7,4 +7,12 @@ export const types = {
   eventClearActiveEvent: '[event] Clear Active Event',
   eventUpdated: '[event] Event updated',
   eventDeleted: '[event] Event deleted',
+
+  authChecking: '[auth] Checking login state',
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authLogout: '[auth] Logout',
+  authStarRegister: '[auth] Start Register',
+  authStartTokenRenew: '[auth] Star Token Renew',
 }
