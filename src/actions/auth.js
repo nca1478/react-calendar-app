97b408cbe225ec41cc8db90750699daf8e3dfe08
Nelson Cadenas <nca1478@yahoo.com>
2022-03-03 18:39:39 -0400

    added: perform fetch to authenticate user

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,30 @@
+import Swal from 'sweetalert2'
+import { types } from '../types/types'
+import { fetchWithoutToken } from '../helpers/fetch'
+
 export const startLogin = (email, password) => {
   return async (dispatch) => {
-    console.log(email, password)
+    const resp = await fetchWithoutToken(
+      'auth/login',
+      { email, password },
+      'POST'
+    )
+    const body = await resp.json()
+
+    if (body.ok) {
+      localStorage.setItem('token', body.token)
+      localStorage.setItem('token-init-date', new Date().getTime())
+
+      dispatch(login({ uid: body.user.uid, name: body.user.name }))
+    } else {
+      Swal.fire('Error', body.msg, 'error')
+    }
+  }
+}
+
+export const login = (user) => {
+  return {
+    type: types.authLogin,
+    payload: user,
   }
 }
