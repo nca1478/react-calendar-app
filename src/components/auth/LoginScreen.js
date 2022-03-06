b5671b61e5a68883242e03bcc403760a95cf0008
Nelson Cadenas <nca1478@yahoo.com>
2022-03-06 01:31:15 -0400

    improved: LoginScreen component

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -38,89 +38,142 @@ export const LoginScreen = () => {
   }
 
   return (
-    <div className="container">
-      <div className="row m-0 vh-100 justify-content-center align-items-center">
-        <div className="col-md-6 login-form-1">
-          <h3>Login</h3>
-          <form onSubmit={handleLogin}>
-            <div className="mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Email"
-                id="lEmail"
-                name="lEmail"
-                value={lEmail}
-                onChange={handleLoginInputChange}
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                name="lPassword"
-                value={lPassword}
-                onChange={handleLoginInputChange}
-              />
-            </div>
-            <div className="mb-3">
-              <input type="submit" className="btnSubmit" value="Login" />
-            </div>
-          </form>
-        </div>
+    <div className="row m-0 vh-100 justify-content-center align-items-center">
+      <div className="col-md-5 col-sm-6">
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+          {/* Login Tab */}
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active"
+              id="login-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#login"
+              type="button"
+              role="tab"
+              aria-controls="login"
+              aria-selected="true"
+            >
+              Login
+            </button>
+          </li>
 
-        <div className="col-md-6 login-form-2">
-          <h3>Register</h3>
-          <form onSubmit={handleRegister}>
-            <div className="mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                name="rName"
-                value={rName}
-                onChange={handleRegisterInputChange}
-                autoComplete="off"
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email"
-                name="rEmail"
-                value={rEmail}
-                onChange={handleRegisterInputChange}
-                autoComplete="off"
-              />
-            </div>
-            <div className="mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-                name="rPassword1"
-                value={rPassword1}
-                onChange={handleRegisterInputChange}
-              />
-            </div>
+          {/* Register Tab */}
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="register-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#register"
+              type="button"
+              role="tab"
+              aria-controls="register"
+              aria-selected="false"
+            >
+              Register
+            </button>
+          </li>
+        </ul>
 
-            <div className="mb-3">
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Confirm Password"
-                name="rPassword2"
-                value={rPassword2}
-                onChange={handleRegisterInputChange}
-              />
+        {/* Tab Content */}
+        <div className="tab-content" id="myTabContent">
+          {/* Login Form */}
+          <div
+            className="tab-pane fade show active"
+            id="login"
+            role="tabpanel"
+            aria-labelledby="login-tab"
+          >
+            <div className="login-form-1">
+              <h3>Calendar App</h3>
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email"
+                    id="lEmail"
+                    name="lEmail"
+                    value={lEmail}
+                    onChange={handleLoginInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="lPassword"
+                    value={lPassword}
+                    onChange={handleLoginInputChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <input type="submit" className="btnSubmit" value="Login" />
+                </div>
+              </form>
             </div>
+          </div>
 
-            <div className="mb-3">
-              <input type="submit" className="btnSubmit" value="Register" />
+          {/* Register Form */}
+          <div
+            className="tab-pane fade"
+            id="register"
+            role="tabpanel"
+            aria-labelledby="register-tab"
+          >
+            <div className="login-form-2">
+              <h3>Register</h3>
+              <form onSubmit={handleRegister}>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    name="rName"
+                    value={rName}
+                    onChange={handleRegisterInputChange}
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Email"
+                    name="rEmail"
+                    value={rEmail}
+                    onChange={handleRegisterInputChange}
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Password"
+                    name="rPassword1"
+                    value={rPassword1}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Confirm Password"
+                    name="rPassword2"
+                    value={rPassword2}
+                    onChange={handleRegisterInputChange}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input type="submit" className="btnSubmit" value="Register" />
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
